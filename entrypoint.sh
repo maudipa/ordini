b12852190ec6849bdeb19e1b02c65d7f4ee68839
maudipa <maurizio.dipasquale@outlook.com>
2025-05-24 22:30:59 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -e
 
-# Copia i file nelle posizioni finali (con sudo se necessario)
-cp /custom-entrypoint/odoo.conf /etc/odoo/ || true
-cp /custom-entrypoint/wait-for-psql.py /usr/local/bin/ || true
+# Copia i file di configurazione (ignora errori se già esistenti)
+cp /custom-entrypoint/odoo.conf /etc/odoo/ 2>/dev/null || true
+cp /custom-entrypoint/wait-for-psql.py /usr/local/bin/ 2>/dev/null || true
 
-# Esegui Odoo
-exec python3 /usr/bin/odoo "$@"
+# Avvia Odoo con parametri espliciti
+exec python3 /usr/bin/odoo \
+  --http-port=${PORT:-8069} \
+  --db_host=${DB_HOST:-False} \
+  --db_port=${DB_PORT:-False} \
+  --db_user=${DB_USER:-odoo} \
+  --db_password=${DB_PASSWORD:-False} \
+  --addons-path=${ADDONS_PATH:-/mnt/extra-addons,/usr/lib/python3/dist-packages/odoo/addons}
