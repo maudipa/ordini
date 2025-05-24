3cbef4ce202092b5a41c18a3cb301723a222cc27
maudipa <maurizio.dipasquale@outlook.com>
2025-05-24 22:18:30 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 set -e
-exec "$@"
+
+# Copia i file nelle posizioni finali (con sudo se necessario)
+cp /custom-entrypoint/odoo.conf /etc/odoo/ || true
+cp /custom-entrypoint/wait-for-psql.py /usr/local/bin/ || true
+
+# Esegui Odoo
+exec python3 /usr/bin/odoo "$@"
